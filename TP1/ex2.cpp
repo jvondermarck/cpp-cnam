1f8c41bef39280244b7e7008dc6c63a34fb013b8
Julien Von Der Marck <vdmjulien05@gmail.com>
2022-09-29 09:42:52 +0200

    Few changes and update

--- TP1/ex2.cpp
+++ TP1/ex2.cpp
@@ -16,18 +16,13 @@ void play_game(int &score_player1, int &score_player2) {
     while(true) {
         std::cout << "--> Match number : " << ++number_matches << std::endl;
         int winner = (rand() % 2); // rand value between 0 and 1
-        if(winner == 0) { // if player1 won the match, we add points in his score
-            add_score(score_player1);
-        } else {
-            add_score(score_player2);
-        }
 
         std::cout << " - Score player 1 : " << score_player1 << std::endl;
         std::cout << " - Score player 2 : " << score_player2 << std::endl;
 
         // if both players have equality
         if(score_player1 == 40 && score_player2 == 40) { // if equality
-            std::cout << "Egalité ! Echange décisif. " << std::endl;
+            std::cout << "Egalite ! Echange decisif. " << std::endl;
             int advantage_player = (rand() % 2); // randomly decide whereas player1 or player2 get the ball advantage
 
             winner = (rand() % 2);
@@ -37,18 +32,24 @@ void play_game(int &score_player1, int &score_player2) {
             } else if(winner == 1 && advantage_player == 1) { //if player2 won this match and got the advantage, he won the game
                 std::cout << "Le gagnant est le joueur 2 !" << std::endl;
                 return;
-            } else { // else it means that it's the player who didn't get the advantage who won, so they're still on equality
-                std::cout << "Vous êtes à égalité encore !" << std::endl;
+            } else { // otherwise it means that it's the player who didn't get the advantage who won, so they're still on equality
+                std::cout << "Vous etes a egalite encore !" << std::endl;
             }
-        } else if(score_player1 == 40) { // if player1 has a score of 40 and player2 a score less than 40 = player 1 won
+        } else if(score_player1 == 40 && winner==0) { // if player1 has a score of 40 and player2 a score less than 40 = player 1 won
             std::cout << "WINNER : Le gagnant est le joueur 1 !" << std::endl;
             return;
-        } else if(score_player2 == 40) { // if player2 has a score of 40 and player1 a score less than 40 = player 2 won
+        } else if(score_player2 == 40 && winner==1) { // if player2 has a score of 40 and player1 a score less than 40 = player 2 won
             std::cout << "WINNER : Le gagnant est le joueur 2 !" << std::endl;
             return;
         } else { // else it means that the game is not yet finished
             std::cout << "Allezzz, on continue le match" << std::endl;
+            if(winner == 0) { // if player1 won the match, we add points in his score
+                add_score(score_player1);
+            } else {
+                add_score(score_player2);
+            }
         }
+        
         std::cout << "" << std::endl;
     }
 }
--- TP1/ex3.cpp
+++ TP1/ex3.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <cctype> // to use toUpper()
 #include <algorithm>
+#include <cstdlib>
+#include <cmath>
+
+
 
 void Ask_Firstname(){
     std::string firstname;
@@ -16,12 +20,12 @@ void Ask_Firstname_V2(){
     std::cout << "What's your full name (name firstname) ? ";
     std::cin >> lastname >> firstname; // ask for firstname and lastname in one single entry 
 
-    if (!lastname.empty()) // Edit : Add the verification
+    if (!lastname.empty()) 
     {
         std::transform(lastname.begin(), lastname.end(), lastname.begin(), ::toupper); // put all the lastname letters in upper
     }
 
-    if (!firstname.empty()) // Edit : Add the verification
+    if (!firstname.empty())
     {
         std::transform(firstname.begin(), firstname.end(), firstname.begin(), ::tolower); // put all the firstname letters in lower
         firstname[0] = std::toupper(firstname[0]); // we put first letter of the firstname in uppercase
@@ -30,12 +34,86 @@ void Ask_Firstname_V2(){
     std::cout << "Welcome, " << lastname << " " << firstname << std::endl;
 }
 
+void Seek_Right_Number() {
+    srand((unsigned) time(0));
+    std::cout << "A number generated will be between 0 and 1000, just find it !" << std::endl;
+    int number_generated = (rand() % 1000);
+    int guessed = -1; // init to -1 because generated value can be from 0 to 1000
+    int number_trials = 0;
+    //std::cout << number_generated << std::endl;
+    
+    do {
+        number_trials++;
+        std::cout << "Enter a number : ";
+        std::cin >> guessed;
+
+        if (std::cin.fail()) {
+            std::cout << "--> Error : please enter a value !" << std::endl;
+            std::cin.clear();
+            std::cin.ignore(); // we clear and ignore the failure to restart the loop
+            continue; // we start from the beginning of the loop
+        }
+
+        if(guessed < number_generated) {
+            std::cout << "It's more !!" << std::endl;
+        } else {
+            std::cout << "It's less !!" << std::endl;
+        }
+    } while(number_generated != guessed);
+    std::cout << "Congrats ! You found the right word in " << number_trials << " trials." << std::endl;
+}
+
+void Seek_Right_Number_Computer() {
+    int number_to_guess; // number entered by the user
+    int number_guessed; // number guessed by the computer
+    int number_trials = 0;
+
+    while(true) {
+        std::cout << "Enter a number between 0 and 1000 to make the computer find it : ";
+        std::cin >> number_to_guess;
+
+        if (std::cin.fail() || number_to_guess < 0 || number_to_guess > 1000) {
+            std::cout << "--> Error : please enter a value !" << std::endl;
+            std::cin.clear();
+            std::cin.ignore(); // we clear and ignore the failure to restart the loop
+            continue; // we start from the beginning of the loop
+        } else {
+            break;
+        }
+    }
+
+    int low = 0;
+    int high = 1000;
+    do {
+        number_trials++;
+        number_guessed = floor((low+high)/2); // Binary search algorithm formula
+        if(number_guessed > number_to_guess) { // it's less 
+            high = number_guessed-1; 
+        } else {
+            low = number_guessed+1; 
+        }
+        std::cout << "Number guessed by the computer : " << number_guessed << std::endl;
+    } while(number_guessed != number_to_guess);
+    std::cout << "Congrats ! The computer found the right word in " << number_trials << " trials." << std::endl;
+}
+
+
 int main(){
     // III.1.1
+    std::cout << "III.1.1" << std::endl;
     Ask_Firstname();
 
     // III.1.2
+    std::cout << "III.1.2" << std::endl;
     Ask_Firstname_V2();
+
+    // III.2.1 + 2.2
+    std::cout << "III.2.1 + 2.2" << std::endl;
+    Seek_Right_Number();
+
+    // III.2.3
+    std::cout << "III.2.3" << std::endl;
+    Seek_Right_Number_Computer();
 
     return 0;
 }
