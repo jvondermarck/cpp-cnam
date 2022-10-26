a8fec6762ad6069312ccc53661fe2dec2991122b
Julien Von Der Marck <vdmjulien05@gmail.com>
2022-10-26 18:37:21 +0200

    feat(main): implémentation du main pour voir tester les classes

--- TP2/src/main.cpp
+++ TP2/src/main.cpp
@@ -1,57 +1,27 @@
-#include <iostream>
 #include "point.hpp"
 #include "rectangle.hpp"
 #include "triangle.hpp"
-#include <string>  
-
-void afficher_triangle(Triangle triangle)
-{
-    std::cout << "Triangle : " << std::endl;
-    std::cout << " - Base : " << triangle.base() << std::endl;
-    std::cout << " - Hauteur : " << triangle.hauteur() << std::endl;
-    std::cout << " - Surface : " << triangle.surface() << std::endl;
-    std::cout << " - Longueur : " << triangle.longueur() << std::endl;
-    std::cout << " - Est rectangle ? : " << triangle.estRectangle() << std::endl;
-    std::cout << " - Est isocèle ? : " << triangle.estIsocele() << std::endl;
-    std::cout << " - Est équilérateral ? : " << triangle.estEquilateral() << std::endl;
-}
+#include "cercle.hpp"
 
 int main()
 {
-    Point A;
-    A.x = 0;
-    A.y = 0;
+    // Rectangle
+    Rectangle rectangle (10, 5);
+    Rectangle grandPerimetre (5, 5);
+    Rectangle grandSurface (6, 6);
+    rectangle.afficher(grandPerimetre, grandSurface);
 
-    Point B;
-    B.x = 0;
-    B.y = 3;
+    // Cercle
+    Point pointCercle = Point {3,3};
+    Point dansCercle = Point{2,2};
+    Point surCercle = Point {3,3};
+    Cercle cercle (pointCercle, 6);
+    cercle.afficher(dansCercle, surCercle);
 
-    Point C;
-    C.x = 4;
-    C.y = 0;
-
+    // Triangle 
+    Point A = Point {0,0};
+    Point B = Point {0,3};
+    Point C = Point {4,0};
     Triangle triangle (A, B, C);
-    afficher_triangle(triangle);
-    
-    A.x = 0;
-    A.y = 0;
-    B.x = 8;
-    B.y = 0;
-    C.x = 4;
-    C.y = 6;
-
-    Triangle triangle2 (A, B, C);
-    afficher_triangle(triangle2);    
-
-    Point d;
-    d.x = 1.5;
-    d.y = 4;
-    Point e;
-    e.x = 1.5;
-    e.y = 2;
-    Point f;
-    f.x = 1.5;
-    f.y = 6;
-    Triangle triangle3 = Triangle(d,e,f);
-    afficher_triangle(triangle3); 
+    triangle.afficher();
 }
